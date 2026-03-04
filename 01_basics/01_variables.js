3b3af7279f08b078afcc4c905b86c2f22f0154ef
Bharath s <bharaths9743@gmail.com>
2026-03-04 15:21:46 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail= "bharaths9@gmail.com"
+var accountPassword ="12345"
+accoutCity = "jaipur"
+
+// accountId  = 2 //not allowed
+accountEmail="hhba@h.com"
+accountPassword="123"
+accoutCity="Bengaluru"
+
+/*
+prefer not to use var
+bcz of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+console.table([accountId,accountPassword,accoutCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict";// treats all js code as newer version
+
+// alert("hello") //
+
+console.log(3+3)
+
+let name="Bharath"
+let age= 20
+let isLoggedIn=false
+
+
