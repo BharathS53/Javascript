df605b294902b3b9f07207d6026480652d73956a
Bharath s <bharaths9743@gmail.com>
2026-03-04 17:33:51 +0000

    add conversion operations for different data types

---
+++ 01_basics/03_converstionOperation.js
@@ -0,0 +1,23 @@
+let  score="33ab "
+console.log(typeof score)
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+/*1->true , 0-> false
+"bharath"->true
+""-> false
+*/
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+let ext=91
+let phone=String(ext)
+console.log(phone)
+console.log(typeof phone)
