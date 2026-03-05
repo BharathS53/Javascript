1a24683d327fe9c38e47d8bfef15996710a6a677
Bharath s <bharaths9743@gmail.com>
2026-03-05 13:29:06 +0000

    math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,23 @@
+// const score=400
+// console.log(score)
+
+// const balance=new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// const otherNumber=123.45026
+// console.log(otherNumber.toPrecision(3));
+// const Number=1000000
+// console.log(Number.toLocalString('en-IN'));
+console.log(Math)
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.9));
+console.log(Math.floor(4.8));
+console.log(Math.min(4,3,2,5));
+console.log(Math.max(4,1,9,8,3,3));
+
+console.log(Math.floor(Math.random(1,6)*10)+1);
+
+
+
