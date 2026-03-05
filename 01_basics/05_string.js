421187df3669d4f6939feb11a8eb6420116e4522
Bharath s <bharaths9743@gmail.com>
2026-03-05 07:56:24 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,12 @@
+const name="bharath"
+const repoCount=50
+
+console.log(name+repoCount+"Value");
+
+console.log(`hello my name is ${name} and my repocount is ${repoCount}`)
+
+const gameName= new String('bharaths')
+console.log(gameName.charAt(2));
+console.log(gameName.toUpperCase());
+console.log(gameName.length)
+
